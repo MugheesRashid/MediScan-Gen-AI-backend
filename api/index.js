f3be0dd8348ff3998e6f6e543c9bf9b0a65437a4
MugheesRashid <512mughees@gmail.com>
2026-06-24 20:56:54 +0500

    This time it will work

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('../app');
-
-// Vercel (and other serverless platforms) will call this function for requests
-// placed under the `/api` path when deploying the `backend` folder as the project root.
-module.exports = (req, res) => {
-  return app(req, res);
-};
